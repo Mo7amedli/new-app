55e188c1bc975c2c078ddbe3c228a90b58a73de8
mo7amedli <moamedli965@gmail.com>
2024-06-23 15:40:02 +0200

    home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css'; // Optional: Your global CSS
-import Profile from './components/Profile';
-
+import MainContent from './components/MainContent';
+import JobPost from './components/JobPost';
+import Freelancers from './components/Freelancers';
+import WebsitePrivileges from './components/WebsitePrivileges';
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-      <Profile />
+      <MainContent/>
+      <JobPost />
+      <Freelancers />
+      <WebsitePrivileges/>
+      <Footer/>
     </div>
   );
 }
